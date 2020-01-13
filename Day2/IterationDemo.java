093a6e171a7a85a47a3c8d4cc7325c01ee972c50
antopravin-ui <59785461+antopravin-ui@users.noreply.github.com>
2020-01-13 16:27:25 -0600

    Add files via upload

---
+++ Day2/IterationDemo.java
@@ -0,0 +1,33 @@
+
+public class IterationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		
+	/*	int i=9 ;
+		while(i<=5) {
+			System.out.println("check the condition");
+			System.out.println("Print value of i " + i);
+			i++;
+		}*/
+		
+		
+	/*	int i=9;
+		do {
+			System.out.println("check the condition");
+			System.out.println("Print value of i " + i);
+			i++;
+		}while (i<=5);
+		*/
+		
+		for(int i=0 ; i<=5 ;i++) {
+			System.out.println("check the condition");
+			System.out.println("Print value of i " + i);
+		}
+		
+		
+		
+	}
+
+}
